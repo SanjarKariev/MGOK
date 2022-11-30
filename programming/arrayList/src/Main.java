d51066942d2db1163df9cc127ed8148350d2706c
SanjarKariev <kariev.s69@mail.ru>
2022-11-30 18:36:34 +0300

    ArrayList+Scanner

--- programming/arrayList/src/Main.java
+++ programming/arrayList/src/Main.java
@@ -1,20 +1,17 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         ArrayList<String> Strings = new ArrayList<String>();
         ArrayList<Integer> Intg = new ArrayList<>();
 
-        Strings.add("Foma");
-        Strings.add("Foma");
-        Intg.add(12);
-
-        Strings.add("Anton");
-        Intg.add(15);
+        Strings.add(scan.nextLine());
+        Intg.add(scan.nextInt());
 
         System.out.println("Name: " + Strings);
         System.out.println("Year: " + Intg);
-        System.out.println(Strings.size());
-        System.out.println(Intg.size());
+
     }
 }
