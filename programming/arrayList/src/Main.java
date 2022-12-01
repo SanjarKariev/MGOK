ef029b5f0dd52c5e6595cdf34bf3ad6686bfb86f
SanjarKariev <kariev.s69@mail.ru>
2022-12-01 08:01:48 +0300

    ArrayList

--- programming/arrayList/src/Main.java
+++ programming/arrayList/src/Main.java
@@ -1,17 +1,26 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList; // Объязательно подключить библеотеку
+import java.util.Scanner; // Объязательно подключить библеотеку
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        ArrayList<String> Strings = new ArrayList<String>();
-        ArrayList<Integer> Intg = new ArrayList<>();
+        Scanner scan = new Scanner(System.in); // Подключение сканера
 
-        Strings.add(scan.nextLine());
-        Intg.add(scan.nextInt());
+        ArrayList<String> Strings = new ArrayList<String>(); // Подключение ArrayList с типом данных String
+        ArrayList<Integer> Intg = new ArrayList<>(); // Подключение ArrayList с типом данных int
 
-        System.out.println("Name: " + Strings);
-        System.out.println("Year: " + Intg);
+        /*Добавление занчений в ArrayList*/
+
+        System.out.println("Input string text: ");
+        Strings.add(scan.nextLine()); // Добавлениеинформации в list с типом данный String, через scanner.nextLine, так как String принемает только текстовые значения
+        System.out.println("Input int number: ");
+        Intg.add(scan.nextInt()); // Добавлениеинформации в list с типом данный int, через scanner.nextInt, так как int принемает только целочисленные значения
+
+        /*Удаление знаяений из List*/
+        Strings.remove(scan.nextLine());
+        Intg.remove(scan.nextInt());
+
+        System.out.println("Name: " + Strings); // Вывод всех добавленных значений в ArrayList<String>
+        System.out.println("Year: " + Intg); // Вывод всех добавленных значений в ArrayList<Integer>
 
     }
 }
